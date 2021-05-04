cf037c34fdd26b1cdb58aa8bacfad74b03f6210a
Nakano Miku <iceice666@outlook.com>
2021-05-04 09:53:31 +0000

    .

--- Power.py
+++ Power.py
@@ -4,6 +4,7 @@ from mcdreforged.minecraft.rtext import RColor, RText, RTextTranslation
 from mcdreforged.plugin.server_interface import ServerInterface
 from mcdreforged.command.builder.command_node import Literal, Number
 
+from time import sleep
 
 import lib.plugin_metadata as pm
 
@@ -53,6 +54,7 @@ INFO_MSG = plugin_info_msg.get_value()
 def on_load(server: ServerInterface, old_module):
 
     process = Process(server)
+    request = Request(server)
 
     # ^ Command System
     server.register_help_message(f'!!{plugin_metadata.get_value("prefix")}',
@@ -69,37 +71,94 @@ def on_load(server: ServerInterface, old_module):
         # & shutdown
         # ~ Close MCDR and Minecraft Server
         then(Literal("shutdown").runs().
-        then(Number("waiting").runs())).
+             then(Number("waiting").runs())).
         # & stop
         # ~ Close Minecraft Server
         then(Literal("stop").runs().
-        then(Number("waiting").runs())).
+             then(Number("waiting").runs())).
         # & start
         # ~ Start Minecraft Server
-        then(Literal("start").runs().
-        then(Number("waiting").runs())).
+        then(Literal("start").runs()).
 
         # & confirm
         then(Literal("confirm").runs()).
         # & cancel
         then(Literal("cancel").runs()).
         # & abort
-        then(Literal("abort").runs())
+        # ! Only server owner or request creator can abort it
+        then(Literal("abort").
+             requires(lambda src: (src.has_permission(4) | src.player == request.Request_info["posted_by"])).
+             runs())
     )
+
 
 # ^ Processing commands here
 
 
 class Process:
-    has_request=[]
+    def __init__(self) -> None:
+        pass
+
+
+class Request:
+    Request_info = {"type": None, "posted_by": ""}
+
     def __init__(self, server):
         self.server = server
 
-    def request(self,src,ctx,type):
-        if src.player in self.has_request:
-            self.server.reply(RText("你有一個請求"))
+    def create(self, src, ctx, type):
+        if self.Request_info["type"] != None:
+            self.server.reply(RText("有已經一個請求了在執行了!!!", RColor=RColor.red))
             return
-        if type=="shutdown":
+
+        self.Request_info["posted_by"] = src.player
+        self.Request_info["type"] = type.lower()
+        if type == "shutdown":
             self._shutdown(ctx["waiting"])
+        elif type == "stop":
+            self._stop(ctx["waiting"])
+        else:
+            self.Request_info = {"type": None, "posted_by": ""}
+            return
 
+    def _shutdown(self, waiting=10):
+        for i in range(waiting):
+            if waiting-i == 60:
+                self.server.boardcast(
+                    RText("Server will shutdown soon! {} secs remaining.".format(waiting-i), RColor=RColor.red))
+            elif waiting-i == 30:
+                self.server.boardcast(
+                    RText("Server will shutdown soon! {} secs remaining.".format(waiting-i), RColor=RColor.red))
+            elif waiting-i == 15:
+                self.server.boardcast(
+                    RText("Server will shutdown soon! {} secs remaining.".format(waiting-i), RColor=RColor.red))
+            elif waiting-i == 10:
+                self.server.boardcast(
+                    RText("Server will shutdown soon! {} secs remaining.".format(waiting-i), RColor=RColor.red))
+            elif waiting-i <= 10:
+                self.server.boardcast(
+                    RText("Countdown {} secs.".format(waiting-i), RColor=RColor.red))
+            elif waiting-i<= 0:
+                self.server.stop_exit()
+            sleep(1)
 
+    def _stop(self, waiting=10):
+        for i in range(waiting):
+            if waiting-i == 60:
+                self.server.boardcast(
+                    RText("Server will stop soon! {} secs remaining.".format(waiting-i), RColor=RColor.red))
+            elif waiting-i == 30:
+                self.server.boardcast(
+                    RText("Server will stop soon! {} secs remaining.".format(waiting-i), RColor=RColor.red))
+            elif waiting-i == 15:
+                self.server.boardcast(
+                    RText("Server will stop soon! {} secs remaining.".format(waiting-i), RColor=RColor.red))
+            elif waiting-i == 10:
+                self.server.boardcast(
+                    RText("Server will stop soon! {} secs remaining.".format(waiting-i), RColor=RColor.red))
+            elif waiting-i <= 10:
+                self.server.boardcast(
+                    RText("Countdown {} secs.".format(waiting-i), RColor=RColor.red))
+            elif waiting-i <= 0:
+                self.server.stop()
+            sleep(1)
